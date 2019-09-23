76d8be549e14fea5fa055c2706f221f730ad5ef9
xelantro <admin@memium.de>
2019-09-23 09:22:07 +0200

    first final release

--- Planeten/src/de/memium/planets/Verwaltung.java
+++ Planeten/src/de/memium/planets/Verwaltung.java
@@ -16,29 +16,30 @@ public class Verwaltung extends JFrame {
 	private static final long serialVersionUID = -5022424809077415287L;
 	private Planet[] karten;
 
-
-
 	public Verwaltung(Planet[] karten) {
 		this.karten=karten;
 
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Verwaltung: Planeten");
-		this.setMinimumSize(new Dimension(800, 400));
 
 		JTable table;
-		Object[][] data = Arrays.stream(karten).map(p-> {Object[] o=new Object[6]; o[0]=p.getName(); o[1]=p.getMasse()+" M⊕"; o[2]=p.getDurchmesser()+" km"; o[3]=p.getUmlaufzeit()+" d"; o[4]=p.getAnzahlMonde()+""; o[5]=p.getAbstandZurSonne()+" Gm";return o;}).toArray(Object[][]::new);
-		Object[] cNames = {"Name","Masse","Durchmesser","Umlaufzeit","Anz. Monde","Abstand zur Sonne"};
+		Object[][] data = Arrays.stream(karten).map(p-> {Object[] o=new Object[6]; o[0]=p.getName(); o[1]=p.getMasse(); o[2]=p.getDurchmesser(); o[3]=p.getUmlaufzeit(); o[4]=p.getAnzahlMonde(); o[5]=p.getAbstandZurSonne(); return o;}).toArray(Object[][]::new);
+		Object[] cNames = {"Name","Masse in M⊕","Durchmesser in km","Umlaufzeit in d","Anz. Monde","Abstand zur Sonne in Gm"};
 		this.add(new JScrollPane(table=new JTable(data, cNames)));
 		table.setFont(new Font(Font.SERIF, Font.PLAIN, 21));
 		table.setRowHeight(30);
 		table.setAutoCreateRowSorter(true);
 		@SuppressWarnings("unchecked")
 		TableRowSorter<TableModel> trs = (TableRowSorter<TableModel>)table.getRowSorter();
-		trs.setComparator(4,(p1,p2)->Integer.parseInt((String) p2)-Integer.parseInt((String) p1));
+		trs.setComparator(1,(Double i, Double j)->i.compareTo(j));
+		trs.setComparator(2,(Double i, Double j)->i.compareTo(j));
+		trs.setComparator(3,(Double i, Double j)->i.compareTo(j));
+		trs.setComparator(4,(Integer i, Integer j)->i.compareTo(j));
+		trs.setComparator(5,(Double i, Double j)->i.compareTo(j));
 
-		//		table.getRowSorter().so
 		//		this.sortieren();
 		//		this.ausgeben();
+		this.setMinimumSize(new Dimension(trs.getModel().getColumnCount()*155, Math.min(500, trs.getModel().getRowCount()*39+70/*ohne Abstand: 10*/)));
 		this.validate();
 		this.setVisible(true);
 	}
